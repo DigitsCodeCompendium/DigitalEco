a5dbe0b48be3906f64d2e3a59561dad3289e8f32
turkeykittin <turkeykittin@gmail.com>
2023-12-28 01:36:29 -0500

    Functional part replacement via inventory

---
+++ src/DE_Maintenance/MaintenanceInventoryComponent.cs
@@ -0,0 +1,141 @@
+namespace Digits.DE_Maintenance
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Eco.Mods.TechTree;
+    using Eco.Core.Items;
+    using Eco.Gameplay.Blocks;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Components.Auth;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Economy;
+    using Eco.Gameplay.Housing;
+    using Eco.Gameplay.Interactions;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Modules;
+    using Eco.Gameplay.Minimap;
+    using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Occupancy;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Property;
+    using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems;
+    using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Gameplay.Pipes.LiquidComponents;
+    using Eco.Gameplay.Pipes.Gases;
+    using Eco.Shared;
+    using Eco.Shared.Math;
+    using Eco.Shared.Localization;
+    using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
+    using Eco.Shared.View;
+    using Eco.Shared.Items;
+    using Eco.Shared.Networking;
+    using Eco.Gameplay.Pipes;
+    using Eco.World.Blocks;
+    using Eco.Gameplay.Housing.PropertyValues;
+    using Eco.Gameplay.Civics.Objects;
+    using Eco.Gameplay.Settlements;
+    using Eco.Gameplay.Systems.NewTooltip;
+    using Eco.Core.Controller;
+    using Eco.Core.Utils;
+	using Eco.Gameplay.Components.Storage;
+    using Eco.Gameplay.Items.Recipes; 
+
+    using static Eco.Gameplay.Items.AuthorizationInventory;
+
+    // This is a good reference component for inventory
+    [Serialized, CreateComponentTabLoc("MaintenanceInventory"), NoIcon]
+    [LocDisplayName("MaintenanceInventory"), LocDescription("Provides maintenance inventory access")]
+    // [NoIcon]
+    [AutogenClass]
+    public class MaintenanceInventoryComponent : WorldObjectComponent, IInventoryWorldObjectComponent
+    {
+        public readonly ThreadSafeAction OnChanged = new();
+
+        public override WorldObjectComponentClientAvailability Availability => WorldObjectComponentClientAvailability.UI;
+        [Serialized, SyncToView, PropReadOnly] public AuthorizationInventory? Inventory { get; private set; }
+        Inventory IInventoryWorldObjectComponent.Inventory => this.Inventory!;
+
+        public MaintenanceInventoryComponent() { }
+
+        public override void Initialize()
+        {
+
+            if (this.Inventory == null)
+                this.Inventory = new AuthorizationInventory(1, AuthorizationFlags.AuthedMayAdd | AuthorizationFlags.AuthedMayRemove, AccessType.FullAccess);
+            this.Inventory.SetOwner(this.Parent);
+            this.Inventory.OnChanged.Add(_ => this.OnChanged.Invoke());
+
+            base.Initialize();
+        }
+        
+        public override void Tick()
+        {
+
+        }
+
+        // // Keeping for easy debugging
+        // // Synthesize into
+        // [RPC, Autogen]
+        // public virtual void SynthesizeIntoInventory(Player player)
+        // {
+        //     if (this.Inventory.NonEmptyStacks.Count() < this.Inventory.Stacks.Count())
+        //     {
+        //         Result result = this.Inventory.TryAddItem(new MachinePartsItem());
+        //         if(result.Success) {
+        //             player.MsgLocStr("<color=green>Synthesized part into inventory");
+        //             return;
+        //         }
+        //     }
+        //     player.MsgLocStr("<color=red>No space in inventory to synthesize!");
+        //     return;
+        // }
+
+        // Pull-out button
+        [RPC, Autogen]
+        public virtual void PullOutPart(Player player)
+        {
+            var itemStacks = this.Inventory.NonEmptyStacks;
+            this.Inventory.MoveAsManyItemsAsPossible(player.User.Inventory, player.User); // This respects inventory space and weight!
+            // TODO Find a way to check this result, but honestly we don't care, it's just doing its best that it can.
+        }
+
+        
+        // Put-in button
+        [RPC, Autogen]
+        public virtual void PutInPart(Player player)
+        {
+            ItemStack itemStack = player.User.Inventory.Toolbar.SelectedStack;
+            var isItemValid = itemStack?.Item != null && itemStack.Item is RepairableMachinePartsItem;
+            if(isItemValid) {
+                Result result = player.User.Inventory.MoveItems(itemStack, this.Inventory, 1); // Try to move item
+                if(result.Success) {
+                    player.MsgLocStr("<color=green>Put in part");
+                } else {
+                    player.MsgLocStr("<color=red>Could not insert part!");
+                }
+            } else {
+                player.MsgLocStr("<color=red>No valid part in hand");
+            }
+        }
+
+        public override InventoryMoveResult TryPickup(Player player, InventoryChangeSet playerInvChanges, Inventory targetInventory, bool force)
+        {
+            // if no modules are installed object can be picked up
+            if (this.Inventory?.IsEmpty != false) return Result.Succeeded;
+
+            // if force is set to true then pick up object and installed modules
+            if (force)
+            {
+                foreach ((var type, var amount) in this.Inventory.TypeToCount)
+                    playerInvChanges.AddItems(type, amount, targetInventory);
+
+                return Result.Succeeded;
+            }
+
+            return playerInvChanges.MoveAsManyItemsAsPossible(this.Inventory, targetInventory); // if we are not forcing, return move result
+        }
+    }
+}
