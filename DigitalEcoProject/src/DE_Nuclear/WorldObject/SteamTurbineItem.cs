b5b326bcb32a3c206aeeff334c7d199440c68fb3
turkeykittin <turkeykittin@gmail.com>
2024-01-02 21:13:03 -0500

    Directory organization and namespaces update

--- DigitalEcoProject/src/DE_Nuclear/WorldObject/SteamTurbineItem.cs
+++ DigitalEcoProject/src/DE_Nuclear/WorldObject/SteamTurbineItem.cs
@@ -1,48 +1,49 @@
-﻿namespace Eco.Mods.TechTree
-{
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using Eco.Core.Items;
-    using Eco.Gameplay.Blocks;
-    using Eco.Gameplay.Components;
-    using Eco.Gameplay.Components.Auth;
-    using Eco.Gameplay.DynamicValues;
-    using Eco.Gameplay.Economy;
-    using Eco.Gameplay.Housing;
-    using Eco.Gameplay.Interactions;
-    using Eco.Gameplay.Items;
-    using Eco.Gameplay.Modules;
-    using Eco.Gameplay.Minimap;
-    using Eco.Gameplay.Objects;
-    using Eco.Gameplay.Occupancy;
-    using Eco.Gameplay.Players;
-    using Eco.Gameplay.Property;
-    using Eco.Gameplay.Skills;
-    using Eco.Gameplay.Systems;
-    using Eco.Gameplay.Systems.TextLinks;
-    using Eco.Gameplay.Pipes.LiquidComponents;
-    using Eco.Gameplay.Pipes.Gases;
-    using Eco.Shared;
-    using Eco.Shared.Math;
-    using Eco.Shared.Localization;
-    using Eco.Shared.Serialization;
-    using Eco.Shared.Utils;
-    using Eco.Shared.View;
-    using Eco.Shared.Items;
-    using Eco.Shared.Networking;
-    using Eco.Gameplay.Pipes;
-    using Eco.World.Blocks;
-    using Eco.Gameplay.Housing.PropertyValues;
-    using Eco.Gameplay.Civics.Objects;
-    using Eco.Gameplay.Settlements;
-    using Eco.Gameplay.Systems.NewTooltip;
-    using Eco.Core.Controller;
-    using Eco.Core.Utils;
-	using Eco.Gameplay.Components.Storage;
-    using static Eco.Gameplay.Housing.PropertyValues.HomeFurnishingValue;
-    using Eco.Gameplay.Items.Recipes;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Eco.Core.Items;
+using Eco.Gameplay.Blocks;
+using Eco.Gameplay.Components;
+using Eco.Gameplay.Components.Auth;
+using Eco.Gameplay.DynamicValues;
+using Eco.Gameplay.Economy;
+using Eco.Gameplay.Housing;
+using Eco.Gameplay.Interactions;
+using Eco.Gameplay.Items;
+using Eco.Gameplay.Modules;
+using Eco.Gameplay.Minimap;
+using Eco.Gameplay.Objects;
+using Eco.Gameplay.Occupancy;
+using Eco.Gameplay.Players;
+using Eco.Gameplay.Property;
+using Eco.Gameplay.Skills;
+using Eco.Gameplay.Systems;
+using Eco.Gameplay.Systems.TextLinks;
+using Eco.Gameplay.Pipes.LiquidComponents;
+using Eco.Gameplay.Pipes.Gases;
+using Eco.Shared;
+using Eco.Shared.Math;
+using Eco.Shared.Localization;
+using Eco.Shared.Serialization;
+using Eco.Shared.Utils;
+using Eco.Shared.View;
+using Eco.Shared.Items;
+using Eco.Shared.Networking;
+using Eco.Gameplay.Pipes;
+using Eco.World.Blocks;
+using Eco.Gameplay.Housing.PropertyValues;
+using Eco.Gameplay.Civics.Objects;
+using Eco.Gameplay.Settlements;
+using Eco.Gameplay.Systems.NewTooltip;
+using Eco.Core.Controller;
+using Eco.Core.Utils;
+using Eco.Gameplay.Components.Storage;
+using static Eco.Gameplay.Housing.PropertyValues.HomeFurnishingValue;
+using Eco.Gameplay.Items.Recipes;
+using Eco.Mods.TechTree;
 
+namespace Digits.Nuclear
+{
     [Serialized]
     [RequireComponent(typeof(AttachmentComponent))]
     //[RequireComponent(typeof(OnOffComponent))]
